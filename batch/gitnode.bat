512af17198fa92d088e4009a8712046e4c8854fd
PowerKuu <hkleven6@outlook.com>
2022-06-24 15:32:12 +0200

    Updated batch file

--- batch/gitnode.bat
+++ batch/gitnode.bat
@@ -6,7 +6,7 @@ set "var=%1"
 if "%var%"=="" set /p "var=Enter entry name (index.ts): " || set "var=index"
 
 echo {"compilerOptions": {"target": "esnext", "lib": ["dom"], "moduleResolution": "node"}}   >> tsconfig.json
-echo {"name":"%var%","version":"1.0.0","description":"","main":"%var%.ts","dependencies":{},"devDependencies":{},"scripts":{"dev": "nodemon --watch './**/*.ts' './**/*.js' --exec npx ts-node %var%.ts"},"author":"","license":"ISC"} >> package.json
+echo {"name":"%var%","version":"1.0.0","description":"","main":"%var%.ts","dependencies":{},"devDependencies":{}, "scripts":{"dev": "nodemon --ext ts,js,json,env --watch *  --exec npm run start", "start": "npx ts-node %var%.ts"},,"author":"","license":"ISC"} >> package.json
 type nul > %var%.ts
 
 npm install typescript @types/node ts-node nodemon lodash
